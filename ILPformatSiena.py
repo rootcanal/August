205a6781bf6b4a28faa00a9e5a42a9642bbd474d
Siena Dumas Ang <SienaDumasAng@d-69-91-211-69.dhcp4.washington.edu>
2015-08-19 14:12:43 -0700

    Siena format file add

---
+++ ILPformatSiena.py
@@ -0,0 +1,85 @@
+import sys
+import makesets
+import pickle
+from random import randint
+import EntityFileCreator as EF
+
+OUT=None
+
+def cleannum(n):
+    return ''.join([x for x in n if x.isdigit() or x=='.' or x=='x' or x=='x*'])
+
+
+def make_eq(q,a,VERBOSE,TRAIN):
+    bigtexamples = {x:([],[]) for x in ["+","*",'/','-','=']}
+    #wps = open(q).readlines()
+    #answs = open(a).readlines()
+    #VERBOSE=True
+    wps = q
+
+    for k in range(len(wps)):
+        if VERBOSE:
+            for i in range(len(wps)):
+                print(i,wps[i])
+            k = int(input())
+        print(k)
+        problem = wps[k]
+        #First preprocessing, tokenize slightly
+        problem = problem.strip().split(" ")
+        for i,x in enumerate(problem):
+            if len(x)==0:continue
+            if x[-1] in [',','.','?']:
+                problem[i] = x[:-1]+" "+x[-1]
+        problem = ' '.join(problem)
+        problem = " " + problem + " "
+        print(problem)
+
+        story = read_parse(k)
+        print(story)
+        exit()
+        sets = makesets.makesets(story['sentences'])
+        EF.main(sets,k,a[k])
+        sets = [x for x in sets if makesets.floatcheck(x[1].num) or x[1].num == 'x']
+        print(sets)
+        for z in sets:
+            z[1].details()
+
+def read_parse(k):
+    return pickle.load(open('s_data/'+str(k)+'.pickle', 'rb'))
+
+
+def parse_inp(inp):
+    q=[]
+    a=[]
+    e=[]
+    with open(inp) as f:
+        f = f.readlines()
+        i=0
+        while i<len(f):
+            q.append(f[i])
+            i+=1
+            e.append(f[i])
+            i+=1
+            a.append(f[i])
+            i+=1
+    return (q,a,e)
+
+
+
+if __name__=="__main__":
+    #q, a = sys.argv[1:3]
+    inp = sys.argv[1]
+    q,a,e = parse_inp(inp)
+    VERBOSE=False
+    TRAIN=False
+    '''
+    if len(sys.argv)>3:
+        if sys.argv[3]=='v':
+            VERBOSE=True
+        elif sys.argv[3]=='t':
+            TRAIN = True
+            OUT = sys.argv[4]
+    '''
+    make_eq(q,a,VERBOSE,TRAIN)
+
+

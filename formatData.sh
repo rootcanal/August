6d51c03b8a76aceeeda807a9824912cf6e679a83
Philip Ramsey <philip@whereikeep.info>
2015-09-12 22:05:45 -0400

    adding a simple check for python 3 version since didn't want to use 3.4 on its own

--- formatData.sh
+++ formatData.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+python_exists ()
+{
+    type $1 &> /dev/null;
+}
 
-python3 split_data.py $1 ;
+if python_exists python3
+then
+    PYTHON=python3
+elif python_exists python3.4
+then
+    PYTHON=python3.4
+else
+    print "must have python3 or python3.4 in PATH" >&2
+    exit 1
+fi
+
+$PYTHON split_data.py $1 ;
 for i in {0..4};
 do
-    python3 fold_train_test.py $i $1 ;
+    $PYTHON fold_train_test.py $i $1 ;
 done
 
 #python3 parseSdata.py $1
